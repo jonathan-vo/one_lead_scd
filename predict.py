24f8e8f281f0f75b020ca8f00ada0cfa89aadc34
jonathan-vo <35127548+jonathan-vo@users.noreply.github.com>
2024-05-31 00:18:58 -0700

    Add files via upload

--- predict.py
+++ predict.py
@@ -24,19 +24,6 @@ targets = pd.read_csv(targets_file)['case']
 
 data_path = project / "test_output"
 
-# weights = torch.load(model_path)
-
-
-# Run = namedtuple("Run", ["name", "data_path", "manifest_path", "weights", "split"])
-# run = [
-#     Run(
-#         "sudden_cardiac_death_final",
-#         project / "test_output",
-#         project / "Manifest/DigitizedECGs_ExternalReplication_5_10_2022_removed.csv",
-#         weights,
-#         None,
-#     )
-# ]
 if __name__ == '__main__':
     test_ds = ECGDataset(
             data_path=data_path,
@@ -50,11 +37,11 @@ if __name__ == '__main__':
     trainer = Trainer()
     classifier = BinaryClassificationModel()
     preds = trainer.predict(model=classifier, ckpt_path=model_path,dataloaders=test_dl)
-    test_df = pd.DataFrame(preds)
+    preds_df = pd.DataFrame(preds)
     
-    ecg_preds_test = np.concatenate((test_df[1][0], test_df[1][1], test_df[1][2]))
-    ecg_name_test = test_df[0][0] + test_df[0][1] +test_df[0][2]
+    ecg_preds = np.concatenate((preds_df[1][0], preds_df[1][1], preds_df[1][2]))
+    ecg_name = preds_df[0][0] + preds_df[0][1] + preds_df[0][2]
     
-    print(f"AUC = {sklearn.metrics.roc_auc_score(targets,ecg_preds_test)}")
-    fpr, tpr, threshold = sklearn.metrics.roc_curve(targets,ecg_preds_test)
+    print(f"AUC = {sklearn.metrics.roc_auc_score(targets,ecg_preds)}")
+    fpr, tpr, threshold = sklearn.metrics.roc_curve(targets,ecg_preds)
     plt.plot(fpr,tpr)
